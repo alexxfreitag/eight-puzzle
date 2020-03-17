6a04ef87aa1c41c5ad85e8eed820a76987c01cda
Alex Eduardo Freitag <alex.f@quantich.com.br>
2020-03-16 22:36:13 -0300

    refactor code

---
+++ src/puzzle/Node.java
@@ -0,0 +1,26 @@
+package puzzle;
+
+import java.util.ArrayList;
+
+
+public class Node {
+	String name;
+	ArrayList<Integer> state;
+	Node parent;
+	int distance;
+	String move;
+	public int priority;
+	
+	public Node(String name){
+		this.name = name;
+	}
+	
+	public Node(){
+		
+	}
+	
+	
+	public String getName(){
+		return this.name;
+	}
+}
